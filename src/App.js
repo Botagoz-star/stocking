fa1f2c067691710aced08fbfa29c7fbe494c435c
Azamat Gaparov <agaparovkg@gmail.com>
2024-01-04 17:26:34 -0800

    added comps

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import FilterableProductTable from "./components/FilterableProductTable";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <FilterableProductTable />
+    </div>
+  );
 }
 
 export default App;
